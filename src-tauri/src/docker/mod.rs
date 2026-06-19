14db805efce70eb1bb0de627698ad2057d9954b7
yuhaoxin <haoxin.yu@outlook.com>
2026-06-19 13:31:11 +0800

    refactor: 移除 Docker 发现模型

--- src-tauri/src/docker/mod.rs
+++ src-tauri/src/docker/mod.rs
@@ -1,9 +1,5 @@
 mod client;
-mod discovery;
 mod error;
-mod types;
 
 pub use client::{verify_local_docker_context, DockerClient};
-pub use discovery::{discover_registry_containers, summarize_container};
 pub use error::DockerError;
-pub use types::{ContainerMount, ContainerPort, RegistryContainerSummary};
